dd8ca3212c42532b64a27875c2ac71e6ffb05600
Sean Hammell <sean.m.hammell@gmail.com>
2023-01-04 16:11:28 -0600

    Add position parsing to UCI

--- uci.c
+++ uci.c
@@ -7,6 +7,19 @@
 #define SFEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 #define ID "id name Omni-Gnome 2.0\nid author Sean Hammell\nuciok\n"
 
+void parsepos(Board *board, char *line)
+{
+    char *cp;
+
+    line += 9;
+    cp = line;
+
+    if (!strncmp(cp, "fen", 3))
+        board_parsefen(board, cp + 4);
+    else
+        board_parsefen(board, SFEN);
+}
+
 void uci_loop(void)
 {
     printf("Omni-Gnome 2.0\n");
@@ -22,10 +35,14 @@ void uci_loop(void)
         if (!fgets(line, MAXBUFF, stdin) || line[0] == '\n')
             continue;
 
-        if (!strncmp(line, "isready", 7))
+        if (!strncmp(line, "ucinewgame", 10))
+            board_parsefen(&board, SFEN);
+        else if (!strncmp(line, "isready", 7))
             printf("readyok\n");
         else if (!strncmp(line, "uci", 3))
             printf(ID);
+        else if (!strncmp(line, "position", 8))
+            parsepos(&board, line);
         else if (!strncmp(line, "display", 7))
             board_display(&board);
         else if(!strncmp(line, "quit", 4))
