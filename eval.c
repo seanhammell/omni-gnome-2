8dcd5e3ebe6b4a8f65432e3d75964774d6414151
Sean Hammell <sean.m.hammell@gmail.com>
2023-01-14 11:32:32 -0600

    Alter pst values based on PeSTO

--- eval.c
+++ eval.c
@@ -1,63 +1,86 @@
 #include "eval.h"
 
-#define MAX(a, b)   (a > b ? a : b)
+static const int values[] = {0, 90, 320, 330, 500, 975};
+static const int gamephase_inc[] = {0, 1, 1, 2, 4, 0};
 
-static const int values[] = {0, 100, 320, 330, 500, 900};
-
-static const int advance_pst[] = {
-     0,   0,   0,   0,   0,   0,   0,   0,
-    50,  50,  50,  50,  50,  50,  50,  50,
-    20,  25,  25,  30,  30,  25,  25,  20,
-    10,  15,  15,  25,  25,  15,  15,  10,
-     0,   0,   0,  20,  20,   0,   0,   0,
-     0,   0,   0,   0,   0,   0,   0,   0,
-     5,  10,  10, -20, -20,  10,  10,   5,
-     0,   0,   0,   0,   0,   0,   0,   0,
+static const int pawn_pst[] = {
+      0,   0,   0,   0,   0,   0,   0,   0,
+    100, 135,  60,  95,  70, 125,  35, -10,
+     -5,   5,  25,  30,  65,  55,  25, -20,
+    -15,  15,   5,  20,  25,  10,  15, -25,
+    -25,   0,  -5,  10,  15,   5,  10, -25,
+    -25,  -5,  -5, -10,   5,   5,  30, -10,
+    -35,   0, -20, -25, -15,  25,  40, -20,
+      0,   0,   0,   0,   0,   0,   0,   0,
 };
 
-static const int center_pst[] = {
-    -40, -30, -20, -20, -20, -20, -30, -40,
-    -30, -20,   0,   0,   0,   0, -20, -30,
-    -20,   0,  10,  15,  15,  10,   0, -20,
-    -20,   0,  15,  20,  20,  15,   0, -20,
-    -20,   0,  15,  20,  20,  15,   0, -20,
-    -20,   0,  10,  15,  15,  10,   0, -20,
-    -30, -20,   0,   0,   0,   0, -20, -30,
-    -40, -30, -20, -20, -20, -20, -30, -40,
+static const int knight_pst[] = {
+    -165, -90, -35, -50,  60, -100, -15, -105,
+     -75, -40,  70,  35,  25,   60,   5,  -15,
+     -45,  60,  35,  65,  85,  130,  75,   45,
+     -10,  15,  20,  55,  35,   70,  20,   20,
+     -15,   5,  15,  15,  30,   20,  20,  -10,
+     -25, -10,  10,  10,  20,   15,  25,  -15,
+     -30, -55, -10,  -5,   0,   20, -15,  -20,
+    -105, -20, -60, -35, -15,  -30, -20,  -25,
 };
 
-static const int diagonal_pst[] = {
-    -20, -10, -10, -10, -10, -10, -10, -20,
-    -10,   0,   0,   0,   0,   0,   0, -10,
-    -10,   0,   5,  10,  10,   5,   0, -10,
-    -10,   5,   5,  10,  10,   5,   5, -10,
-    -10,   5,  10,  10,  10,  10,   5, -10,
-    -10,  10,  10,  15,  15,  10,  10, -10,
-    -10,  10,   0,   0,   0,   0,  10, -10,
-    -20, -10, -10, -10, -10, -10, -10, -20,
+static const int bishop_pst[] = {
+    -30,   5, -80, -35, -25, -40,   5, -10,
+    -25,  15, -20, -15,  30,  60,  20, -45,
+    -15,  35,  45,  40,  35,  50,  40,   0,
+     -5,   5,  20,  50,  35,  35,   5,   0,
+     -5,  15,  15,  25,  35,  10,  10,   5,
+      0,  15,  15,  15,  15,  25,  20,  10,
+      5,  15,  15,   0,   5,  20,  35,   0,
+    -35,  -5, -15, -20, -15, -10, -40, -20,
 };
 
-static const int back_rank_pst[] = {
-     0,  10,  10,  10,  10,  10,  10,   0,
-    10,  20,  20,  20,  20,  20,  20,  10,
-     0,  10,  10,  10,  10,  10,  10,   0,
-     0,   0,   0,   0,   0,   0,   0,   0,
-     0,   0,   0,   0,   0,   0,   0,   0,
-     0,   0,   0,   0,   0,   0,   0,   0,
-     0,   0,   0,   0,   0,   0,   0,   0,
-     0,   0,   0,   0,   0,   0,   0,   0,
+static const int rook_pst[] = {
+     30,  40,  30,  50, 65, 10,  30,  45,
+     25,  30,  60,  60, 80, 65,  25,  45,
+     -5,  20,  25,  35, 15, 45,  60,  15,
+    -25, -10,   5,  25, 25, 35, -10, -20,
+    -35, -25, -10,   0, 10, -5,   5, -25,
+    -45, -25, -15, -15,  5,  0,  -5, -35,
+    -45, -15, -20, -10,  0, 10,  -5, -70,
+    -20, -15,   0,  15, 15,  5, -35, -25,
 };
 
-static const int king_safety_pst[] = {
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -20, -30, -30, -40, -40, -30, -30, -20,
-    -10, -20, -20, -20, -20, -20, -20, -10,
-     20,  20,   0,   0,   0,   0,  20,  20,
-     20,  30,  10,   0,   0,  10,  30,  20,
+static const int queen_pst[] = {
+    -30,   0,  30,  10,  60,  45,  45,  45,
+    -25, -40,  -5,   0, -15,  55,  30,  55,
+    -15, -15,   5,  10,  30,  55,  45,  55,
+    -25, -25, -15, -15,   0,  15,   0,   0,
+    -10, -25, -10, -10,   0,  -5,   5,  -5,
+    -15,   0, -10,   0,  -5,   0,  15,   5,
+    -35, -10,  10,   0,  10,  15,  -5,   0,
+      0, -20, -10,  10, -15, -25, -30, -50,
 };
+
+static const int king_mg_pst[] = {
+    -65,  25,  15, -15, -55, -35,   0,  15,
+     30,   0, -20,  -5, -10,  -5, -40, -30,
+    -10,  25,   0, -15, -20,   5,  20, -20,
+    -15, -20, -10, -25, -30, -25, -15, -35,
+    -50,   0, -25, -40, -45, -45, -35, -50,
+    -15, -15, -20, -45, -45, -30, -15, -25,
+      0,   5, -10, -65, -45, -15,  10,  10,
+    -15,  35,  10, -55,  10, -30,  25,  15,
+};
+
+static const int king_eg_pst[] = {
+    -75, -35, -20, -20, -10,  15,   5, -15,
+    -10,  15,  15,  15,  15,  40,  25,  10,
+     10,  15,  25,  15,  20,  45,  45,  15,
+    -10,  20,  25,  25,  25,  35,  25,   5,
+    -20,  -5,  20,  25,  25,  25,  10, -10,
+    -20,  -5,  10,  20,  25,  15,   5, -10,
+    -25, -10,   5,  15,  15,   5,  -5, -15,
+    -55, -35, -21, -10, -30, -15, -25, -45,
+};
+
+static int gamephase;
 
 /**
  * material_value
@@ -66,14 +89,16 @@ static const int king_safety_pst[] = {
 int material_value(const Board *board)
 {
     int i, m_value;
-    U64 allybb, enemybb;
+    U64 allybb, ally_pop;
+    U64 enemybb, enemy_pop;
 
-    m_value = 0;
+    m_value = gamephase = 0;
     for (i = 1; i < 7; ++i) {
         allybb = board->piecebb[i] & board->colorbb[board->side];
         enemybb = board->piecebb[i] & board->colorbb[board->side ^ 1];
         m_value += POPCNT(allybb) * values[i];
         m_value -= POPCNT(enemybb) * values[i];
+        gamephase += POPCNT(board->piecebb[i]) * gamephase_inc[i];
     }
     return m_value;
 }
@@ -181,6 +206,7 @@ int knight_mobility(const Board *board, U64 allies, U64 enemies)
     return nm_value * 5;
 }
 
+#define MAX(a, b)   (a > b ? a : b)
 /**
  * long_diagonal
  *  give a bonus for bishops on long, open diagonals
@@ -247,7 +273,7 @@ int pawn_score(const Board *board)
     allies = board->piecebb[PAWN] & board->colorbb[board->side];
     enemies = board->piecebb[PAWN] & board->colorbb[board->side ^ 1];
 
-    p_value = square_bonus(board, PAWN, advance_pst);
+    p_value = square_bonus(board, PAWN, pawn_pst);
     p_value += blocked_pawns(board, allies, enemies);
     p_value += doubled_isolated_pawns(board, allies, enemies);
     return p_value;
@@ -265,7 +291,7 @@ int knight_score(const Board *board)
     allies = board->piecebb[KNIGHT] & board->colorbb[board->side];
     enemies = board->piecebb[KNIGHT] & board->colorbb[board->side ^ 1];
 
-    n_value = square_bonus(board, KNIGHT, center_pst);
+    n_value = square_bonus(board, KNIGHT, knight_pst);
     n_value += knight_mobility(board, allies, enemies);
     return n_value;
 }
@@ -287,7 +313,7 @@ int bishop_score(Board *board)
     if (POPCNT(enemies) > 1)
         b_value -= 30;
 
-    b_value = square_bonus(board, BISHOP, diagonal_pst);
+    b_value = square_bonus(board, BISHOP, bishop_pst);
     b_value += long_diagonal(board, allies, enemies);
     return b_value;
 }
@@ -304,9 +330,25 @@ int rook_score(Board *board)
     allies = board->piecebb[ROOK] & board->colorbb[board->side];
     enemies = board->piecebb[ROOK] & board->colorbb[board->side ^ 1];
 
-    r_value = square_bonus(board, ROOK, back_rank_pst);
+    r_value = square_bonus(board, ROOK, rook_pst);
     r_value += open_file(board, allies, enemies);
     return r_value;
+}
+
+/**
+ * queen_score
+ *  evaluate queens based on standing
+ */
+int queen_score(Board *board)
+{
+    int q_value;
+    U64 allies, enemies;
+
+    allies = board->piecebb[QUEEN] & board->colorbb[board->side];
+    enemies = board->piecebb[QUEEN] & board->colorbb[board->side ^ 1];
+
+    q_value = square_bonus(board, QUEEN, queen_pst);
+    return q_value;
 }
 
 /**
@@ -316,18 +358,18 @@ int rook_score(Board *board)
  */
 int king_score(Board *board)
 {
-    int k_value;
+    int k_mg_value, k_eg_value;
+    int mg_phase, eg_phase;
     U64 allies, enemies;
 
     allies = board->piecebb[KING] & board->colorbb[board->side];
     enemies = board->piecebb[KING] & board->colorbb[board->side ^ 1];
 
-    k_value = 0;
-    if (board->piecebb[QUEEN])
-        k_value += square_bonus(board, KING, king_safety_pst);
-    else
-        k_value += square_bonus(board, KING, center_pst);
-    return k_value;
+    k_mg_value = square_bonus(board, KING, king_mg_pst);
+    k_eg_value = square_bonus(board, KING, king_eg_pst);
+    mg_phase = gamephase > 24 ? 24 : gamephase;
+    eg_phase = 24 - mg_phase;
+    return (k_mg_value * mg_phase + k_eg_value * eg_phase) / 24;
 }
 
 /**
@@ -343,6 +385,7 @@ int eval_heuristic(Board *board)
     eval += knight_score(board);
     eval += bishop_score(board);
     eval += rook_score(board);
+    eval += queen_score(board);
     eval += king_score(board);
     return eval;
 }
